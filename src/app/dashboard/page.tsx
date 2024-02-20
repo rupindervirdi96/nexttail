647368c946d14a7ed5281e310d92125927344284
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2024-02-20 16:26:35 -0500

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Dashboard() {
+    return (
+      <main className="">
+        <ul className="text-white">
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+            <li>hello</li>
+        </ul>
+      </main>
+    );
+  }
+  
